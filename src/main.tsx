59d1ec3c4d3cc6334f429f5f24e059164672d35c
Anthony Chen <anthonychen@anthonys-laptop.mynetworksettings.com>
2024-06-14 20:24:35 -0400

    Changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, createHashRouter } from 'react-router-dom';
+import {  RouterProvider, createHashRouter } from 'react-router-dom';
 import Home from './components/Home';
 import './index.css'
 
