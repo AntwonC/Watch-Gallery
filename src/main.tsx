fca32465280a8ea0446595f2b12de0461872303c
Anthony Chen <anthonychen@anthonys-laptop.mynetworksettings.com>
2024-06-14 20:18:57 -0400

    Changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, createHashRouter } from 'react-router-dom';
 import Home from './components/Home';
 import './index.css'
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
-    path: "/",
+    path: "/Watch-Gallery",
     element: <Home />
   }
 ]);
